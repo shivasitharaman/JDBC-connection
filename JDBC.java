b8d8526e3f7220760d45cc5ea9275d80eb6f0267
shivasitharaman <40603401+shivasitharaman@users.noreply.github.com>
2018-08-12 21:59:16 +0530

    Add files via upload

---
+++ JDBC.java
@@ -0,0 +1,22 @@
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+public class JDBC {
+
+	public static void main(String[] args) {
+		try
+		{
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection conn = null;
+		conn = DriverManager.getConnection("jdbc:mysql://localhost/test","root", "");
+		System.out.print("Database is connected !");
+		conn.close();
+		}
+		catch(Exception e)
+		{
+		System.out.print("Do not connect to DB - Error:"+e);
+		}
+
+	}
+
+}
